78f41e9f453e16877e6b15d126462dc223ebb1d3
Niravkumar Patel <hn.patel@outlook.com>
2025-07-18 10:55:34 -0500

    cochat

---
+++ src/app/chat-assistant/chat-assistant.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-assistant',
+  imports: [],
+  templateUrl: './chat-assistant.html',
+  styleUrl: './chat-assistant.css'
+})
+export class ChatAssistant {
+
+}
